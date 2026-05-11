6803d25dba73b2fa694a8fbbf3909e8fc64d14c1
Oshana Kavinash 0743563623 <oshanachamuditha@gmail.com>
2026-05-11 19:11:47 +0530

    Search and Clear Buttons Test

--- src/test/java/Enrollments.java
+++ src/test/java/Enrollments.java
@@ -51,4 +51,29 @@ public class Enrollments {
         WebElement sub=driver.findElement(By.name("enroll"));
         sub.click();
     }
+
+    @Test
+    public void searchTest() throws InterruptedException {
+
+
+        WebElement dropDown1 = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div[1]/form/select"));
+        dropDown1.click();
+        List<WebElement> options1 = dropDown1.findElements(By.tagName("option"));
+        for (WebElement element : options1) {
+            String dropDownValue = element.getText();
+            if (dropDownValue.equals("Ashan Bandara")) {
+                element.click();
+                break;
+            }
+        }
+
+        Thread.sleep(3000);
+
+        WebElement search=driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div[1]/form/button"));
+        search.click();
+        Thread.sleep(3000);
+
+        WebElement clear=driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div[1]/form/a"));
+        clear.click();
+    }
 }
