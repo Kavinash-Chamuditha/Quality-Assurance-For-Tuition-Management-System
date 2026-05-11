5499b3269e9cc74255449ad9f5e5b242be907aaa
Oshana Kavinash 0743563623 <oshanachamuditha@gmail.com>
2026-05-11 18:01:54 +0530

    add teacher Test

---
+++ src/test/java/Teachers.java
@@ -0,0 +1,59 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Teachers {
+
+    WebDriver driver;
+
+        @Test
+        public void OpenTestPage(){
+            driver =  new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("http://localhost:3000/teachers.php");
+        }
+
+
+        @Test
+        public void addTeacherTest() throws InterruptedException {
+
+            WebElement fullName = driver.findElement(By.name("full_name"));
+            fullName.sendKeys("Kosala Pushpakumara");
+            Thread.sleep(3000);
+
+            WebElement email = driver.findElement(By.name("email"));
+            email.sendKeys("kosala@gmail.com");
+            Thread.sleep(3000);
+
+            WebElement phone = driver.findElement(By.name("phone"));
+            phone.sendKeys("0778877123");
+            Thread.sleep(3000);
+
+            WebElement dropDown = driver.findElement(By.name("subject"));
+            dropDown.click();
+            List<WebElement> options = dropDown.findElements(By.tagName("option"));
+            for (WebElement element : options) {
+                String dropDownValue = element.getText();
+                if (dropDownValue.equals("Science")) {
+                    element.click();
+                    break;
+                }
+            }
+            Thread.sleep(3000);
+
+            WebElement address = driver.findElement(By.name("salary"));
+            address.sendKeys("45000.00");
+            Thread.sleep(3000);
+
+            WebElement addStudent=driver.findElement(By.name("add_teacher"));
+            addStudent.click();
+
+        }
+
+
+    }
