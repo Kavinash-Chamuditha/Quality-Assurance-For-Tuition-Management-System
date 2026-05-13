9c165e9e6f697c54d8e474d385f2f5bb9eadcdba
Oshana Kavinash 0743563623 <oshanachamuditha@gmail.com>
2026-05-13 18:38:53 +0530

    Payment History Screenshot

--- src/test/java/Dashboard.java
+++ src/test/java/Dashboard.java
@@ -1,10 +1,12 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class Dashboard {
 
@@ -12,8 +14,8 @@ public class Dashboard {
     JavascriptExecutor jsExecutor;
 
     @BeforeMethod
-    public void OpenTestPage(){
-        driver =  new ChromeDriver();
+    public void OpenTestPage() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://localhost:3000/index.php");
         jsExecutor = (JavascriptExecutor) driver;
@@ -23,13 +25,13 @@ public class Dashboard {
     public void buttonTest() throws InterruptedException {
 
         Thread.sleep(3000);
-        WebElement view=driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/div/a"));
+        WebElement view = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/div/a"));
         view.click();
         Thread.sleep(3000);
         driver.navigate().back();
         Thread.sleep(3000);
 
-        WebElement manage=driver.findElement(By.xpath("/html/body/div[2]/div[4]/div/a"));
+        WebElement manage = driver.findElement(By.xpath("/html/body/div[2]/div[4]/div/a"));
         manage.click();
         Thread.sleep(3000);
         driver.navigate().back();
@@ -49,8 +51,8 @@ public class Dashboard {
 
     }
 
-@Test
-    public void recent_enrollmentTableTest(){
+    @Test
+    public void recent_enrollmentTableTest() {
 
         int rowCount = driver.findElements(
                 By.xpath("/html/body/div[2]/div[3]/div[1]/table/tbody/tr")).size();
@@ -62,15 +64,14 @@ public class Dashboard {
         System.out.println("Column count is: " + columnCount);
 
 
-        String value=driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/table/tbody/tr[3]")).getText();
-        System.out.println("Specified row/column data" +value);
+        String value = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/table/tbody/tr[3]")).getText();
+        System.out.println("Specified row/column data" + value);
 
     }
 
 
-
     @Test
-    public void Classes_OverviewTableTest(){
+    public void Classes_OverviewTableTest() {
 
         int rowCount = driver.findElements(
                 By.xpath("/html/body/div[2]/div[4]/table/tbody/tr")).size();
@@ -82,8 +83,19 @@ public class Dashboard {
         System.out.println("Column count is: " + columnCount);
 
 
-        String value=driver.findElement(By.xpath("/html/body/div[2]/div[4]/table/tbody/tr[3]")).getText();
-        System.out.println("Specified row/column data" +value);
+        String value = driver.findElement(By.xpath("/html/body/div[2]/div[4]/table/tbody/tr[3]")).getText();
+        System.out.println("Specified row/column data" + value);
 
+    }
+
+    @Test
+    public void screenshot() throws IOException {
+
+        WebElement section = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[2]"));
+        File sourceFile = section.getScreenshotAs(OutputType.FILE);
+        File destinationFile = new File("C:\\Users\\oshan\\IdeaProjects\\QA-Testing\\Screenshots\\Section_" + System.currentTimeMillis() + ".png");
+        Files.copy(sourceFile.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        System.out.println("Screenshot saved to: " + destinationFile.getAbsolutePath());
     }
 }
